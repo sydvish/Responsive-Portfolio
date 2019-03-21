d469e1bf31b6ef1bbe9bb63d4e27d77198859a49
sydney Volk <sydneyvolk@sydneys-MacBook-Pro.local>
2019-03-20 19:43:40 -0700

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $(".w3-bar-item").on("click", function(e){
+  $(".w3-bar-item w3 button").on("click", function(e){
     var $target = $($(this).attr("data-target"));
     $("html, body").animate(
       {
